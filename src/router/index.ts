6ae5a728e6a7a761eb38e5ae70c49c62ab84d6d2
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-11-11 08:12:43 +0100

    complete Lazy Load Routes with Vite’s Dynamic Imports in Vue Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/components/HomeView.vue'
+import ProjectsView from '@/components/ProjectsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: ProjectsView,
+    },
+  ],
 })
 
 export default router
