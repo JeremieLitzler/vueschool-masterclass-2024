7cf22f3bc77f0d7b3703470a3953e23b7556023e
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-11-13 13:18:43 +0100

    complete Set Up Auto Routes in Vue Router with TypeScript Support

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,43 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import { h } from 'vue'
+// the import below requires the following line
+// to be added to `env.d.ts` to register the global
+// typed in the IDE.
+// otherwise, there will be an TS error.
+//
+// <reference types="unplugin-vue-router/client" />
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/ProjectsView.vue'),
-    },
-    {
-      path: '/projects/:id',
-      name: 'project',
-      component: () => import('@/views/SingleProjectView.vue'),
-    },
-
-    {
-      //catchAll is a wild card predefined in Vue to match all possible paths
-      // (.*) matches any characters
-      // * matches the previous patterns as many times as necessary
-      path: '/:catchAll(.*)*',
-      name: 'not-found',
-      component: h('p', { style: 'color:red' }, '404 Not found'),
-    },
-    {
-      //catchAll is a wild card predefined in Vue to match all possible paths
-      // (.*) matches any characters
-      // * matches the previous patterns as many times as necessary
-      path: '/projects/:catchAll(.*)*',
-      name: 'project-not-found',
-      component: h('p', { style: 'color:red' }, 'Project not found'),
-    },
-  ],
+  routes,
 })
 
 export default router
