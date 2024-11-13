f2b172c56c389ba30761ee8a46a8a8aa36e0b99c
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-11-13 13:01:23 +0100

    complete Catch All Undefined Routes and Create 404 Not Found Page in Vue Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import { h } from 'vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,23 @@ const router = createRouter({
       path: '/projects/:id',
       name: 'project',
       component: () => import('@/views/SingleProjectView.vue'),
+    },
+
+    {
+      //catchAll is a wild card predefined in Vue to match all possible paths
+      // (.*) matches any characters
+      // * matches the previous patterns as many times as necessary
+      path: '/:catchAll(.*)*',
+      name: 'not-found',
+      component: h('p', { style: 'color:red' }, '404 Not found'),
+    },
+    {
+      //catchAll is a wild card predefined in Vue to match all possible paths
+      // (.*) matches any characters
+      // * matches the previous patterns as many times as necessary
+      path: '/projects/:catchAll(.*)*',
+      name: 'project-not-found',
+      component: h('p', { style: 'color:red' }, 'Project not found'),
     },
   ],
 })
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Ops... You got lost.</h1>
+  <p>Let's take you back</p>
+  <router-link :to="{ name: 'home' }">Go home</router-link>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
