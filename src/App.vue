f04bfe335237e10f63ce976b9534859b86a5530f
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-11-15 13:08:27 +0100

    complete Create a Sidebar and Organize Code with Vue Components

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,15 @@
 <script setup lang="ts">
-import Avatar from './components/ui/avatar/Avatar.vue'
-import AvatarFallback from './components/ui/avatar/AvatarFallback.vue'
-import AvatarImage from './components/ui/avatar/AvatarImage.vue'
-import DropdownMenu from './components/ui/dropdown-menu/DropdownMenu.vue'
-import DropdownMenuContent from './components/ui/dropdown-menu/DropdownMenuContent.vue'
-import DropdownMenuItem from './components/ui/dropdown-menu/DropdownMenuItem.vue'
-import DropdownMenuLabel from './components/ui/dropdown-menu/DropdownMenuLabel.vue'
-import DropdownMenuSeparator from './components/ui/dropdown-menu/DropdownMenuSeparator.vue'
-import DropdownMenuTrigger from './components/ui/dropdown-menu/DropdownMenuTrigger.vue'
-import Input from './components/ui/input/Input.vue'
+import NavBarTop from './components/layout/NavBarTop.vue'
+import SideBar from './components/layout/SideBar.vue'
 </script>
 
 <template>
-  <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
-    <form class="relative w-full h-fit max-w-96">
-      <iconify-icon
-        class="absolute top-[50%] translate-y-[-50%] left-3 text-muted-foreground"
-        icon="lucide:search"
-      ></iconify-icon>
-      <Input class="w-full pl-8 bg-background" type="text" placeholder="Search..." />
-    </form>
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar>
-          <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
-          <AvatarFallback>JL</AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>Label</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Item 1</DropdownMenuItem>
-        <DropdownMenuItem>Item 2</DropdownMenuItem>
-        <DropdownMenuItem>Item 3</DropdownMenuItem>
-        <DropdownMenuItem>Item 4</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </nav>
-  <main>
-    <RouterView />
-  </main>
+  <SideBar />
+  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
+    <NavBarTop />
+    <main>
+      <RouterView />
+    </main>
+    <footer></footer>
+  </div>
 </template>
