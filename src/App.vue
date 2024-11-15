d49d0f83c637de1006f8022c8c16f836884c628a
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-11-15 16:00:22 +0100

    complete Create Layout Vue.js Component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
-import NavBarTop from './components/layout/NavBarTop.vue'
-import SideBar from './components/layout/SideBar.vue'
+import AuthLayout from './components/layout/main/AuthLayout.vue'
 </script>
 
 <template>
-  <SideBar />
-  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
-    <NavBarTop />
-    <main>
-      <RouterView />
-    </main>
-    <footer></footer>
-  </div>
+  <AuthLayout>
+    <RouterView />
+  </AuthLayout>
 </template>
