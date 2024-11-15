ae09b06f1c9f9a2c421ee812616099ea1f22399c
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-11-15 16:17:38 +0100

    complete Build a Vue.js Data Table Component with Shadcn and TanStack

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1>All tasks</h1>
-    <ul>
-      <li v-for="task in tasks" :key="task?.id">{{ task?.name }}</li>
-    </ul>
-  </div>
+  <DataTable :columns="columns" :data="payments" />
 </template>
 
 <script setup lang="ts">
@@ -22,6 +17,48 @@ const tasks = ref<Tables<'tasks'>[] | null>(null)
 
   tasks.value = data
 })()
+
+import { h } from 'vue'
+import type { ColumnDef } from '@tanstack/vue-table'
+import DataTable from '@/components/ui/data-table/DataTable.vue'
+
+interface Payment {
+  id: string
+  amount: number
+  status: 'pending' | 'processing' | 'success' | 'failed'
+  email: string
+}
+
+const payments: Payment[] = [
+  {
+    id: '728ed52f',
+    amount: 100,
+    status: 'pending',
+    email: 'm@example.com',
+  },
+  {
+    id: '489e1d42',
+    amount: 125,
+    status: 'processing',
+    email: 'example@gmail.com',
+  },
+  // ...
+]
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'amount',
+    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    cell: ({ row }) => {
+      const amount = Number.parseFloat(row.getValue('amount'))
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(amount)
+
+      return h('div', { class: 'text-right font-medium' }, formatted)
+    },
+  },
+]
 </script>
 
 <style scoped></style>
