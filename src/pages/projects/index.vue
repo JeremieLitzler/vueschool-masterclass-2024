77fbf646fa8141076b104b216c90d60707a5923e
Jeremie Litzler <jlitzler@cbtw.tech>
2024-12-12 09:10:50 +0100

    complete Create a Data Table for the Projects Page

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
     <h1>All projects</h1>
-    <ul>
-      <li v-for="project in projects" :key="project?.id">{{ project?.name }}</li>
-    </ul>
+    <DataTable v-if="projects" :columns="columns" :data="projects" />
   </div>
 </template>
 
@@ -22,6 +20,38 @@ const projects = ref<Tables<'projects'>[] | null>(null)
 
   projects.value = data
 })()
+
+import { h } from 'vue'
+import type { ColumnDef } from '@tanstack/vue-table'
+import DataTable from '@/components/ui/data-table/DataTable.vue'
+
+const columns: ColumnDef<Tables<'projects'>>[] = [
+  {
+    accessorKey: 'name',
+    header: () => h('div', { class: 'text-left' }, 'Name'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('name'))
+    },
+  },
+  {
+    accessorKey: 'status',
+    header: () => h('div', { class: 'text-left' }, 'Status'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
+    },
+  },
+  {
+    accessorKey: 'collaborators',
+    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
+    cell: ({ row }) => {
+      return h(
+        'div',
+        { class: 'text-left font-medium' },
+        JSON.stringify(row.getValue('collaborators')),
+      )
+    },
+  },
+]
 </script>
 
 <style scoped></style>
