8e7cf62fe0fbe853fb18cd1225d23df311e8520d
Jeremie Litzler <jlitzler@cbtw.tech>
2024-12-18 11:52:08 +0100

    complete Configure FormKit in Vue.js 3

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,3 +1,4 @@
 import { defaultConfig } from '@formkit/vue'
+import { rootClasses } from './formkit.theme'
 
-export default defaultConfig({})
+export default defaultConfig({ config: { rootClasses } })
