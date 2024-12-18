a837f9c55dbf5dc15097542b12eed2fe2f271076
Jeremie Litzler <jlitzler@cbtw.tech>
2024-12-18 11:48:31 +0100

    complete How to Install and Use FormKit in Vue.js 3

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import 'iconify-icon'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { plugin } from '@formkit/vue'
+import defaultConfig from '../formkit.config'
 import App from './App.vue'
 import router from './router'
 
@@ -11,6 +12,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+// formKit
+app.use(plugin, defaultConfig)
 
 app.config.errorHandler = (error) => console.error(error)
 
