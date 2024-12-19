8263f4c227915bfc2234d6dda7a63cd3a8d084d9
Jeremie Litzler <jlitzler@cbtw.tech>
2024-12-19 16:07:13 +0100

    complete Add Dark Mode Toggle in Vue With useDark from VueUse

--- src/components/layout/NavBarTop.vue
+++ src/components/layout/NavBarTop.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+const authStore = useAuthStore()
+const { profile } = storeToRefs(authStore)
+
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
 <template>
   <nav class="h-16 border-b bg-muted/40 flex gap-2 justify-between px-6 items-center">
     <form class="relative h-fit w-full max-w-96">
@@ -7,30 +16,35 @@
       ></iconify-icon>
       <Input class="w-full pl-8 bg-background" type="text" placeholder="Search ..." />
     </form>
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <AppAvatar :profile="profile" />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem
-          ><RouterLink
-            :to="{ name: '/profiles/[username]', params: { username: profile?.username! } }"
-            >Profile</RouterLink
-          ></DropdownMenuItem
-        >
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div class="flex justify-center items-center gap-1">
+      <Button @click="toggleDark()" class="w-8 h-8">
+        <Transition name="scale" mode="out-in">
+          <iconify-icon v-if="isDark" class="" icon="lucide:sun"></iconify-icon>
+          <iconify-icon v-else class="" icon="lucide:moon"></iconify-icon>
+        </Transition>
+      </Button>
+      <div class="w-8">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <AppAvatar :profile="profile" />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              ><RouterLink
+                :to="{ name: '/profiles/[username]', params: { username: profile?.username! } }"
+                >Profile</RouterLink
+              ></DropdownMenuItem
+            >
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
   </nav>
 </template>
-
-<script setup lang="ts">
-const authStore = useAuthStore()
-const { profile } = storeToRefs(authStore)
-</script>
 
 <style scoped></style>
